8bde2fe1da9f1bca5805a79b244d5c0761f10d03
Tran Anh Hao <Trananhhao1307@gmail.com>
2024-10-10 16:19:41 +0700

    Update: Member update & remove

--- convex/members.ts
+++ convex/members.ts
@@ -1,7 +1,8 @@
 import { v } from 'convex/values';
-import { query, QueryCtx } from './_generated/server';
+import { mutation, query, QueryCtx } from './_generated/server';
 import { getAuthUserId } from '@convex-dev/auth/server';
 import { Id } from './_generated/dataModel';
+import { auth } from './auth';
 
 const populateUser = (ctx: QueryCtx, id: Id<'users'>) => {
   return ctx.db.get(id);
@@ -15,7 +16,9 @@ export const get = query({
 
     const member = await ctx.db
       .query('members')
-      .withIndex('by_workspace_id_user_id', (q) => q.eq('workspaceId', args.workspaceId).eq('userId', userId))
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', args.workspaceId).eq('userId', userId)
+      )
       .unique();
 
     if (!member) return [];
@@ -24,7 +27,9 @@ export const get = query({
 
     const data = await ctx.db
       .query('members')
-      .withIndex('by_workspace_id', (q) => q.eq('workspaceId', args.workspaceId))
+      .withIndex('by_workspace_id', (q) =>
+        q.eq('workspaceId', args.workspaceId)
+      )
       .collect();
 
     for (const member of data) {
@@ -48,7 +53,9 @@ export const current = query({
 
     const member = await ctx.db
       .query('members')
-      .withIndex('by_workspace_id_user_id', (q) => q.eq('workspaceId', args.workspaceId).eq('userId', userId))
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', args.workspaceId).eq('userId', userId)
+      )
       .unique();
 
     if (!member) return null;
@@ -71,7 +78,9 @@ export const getById = query({
 
     const currentMember = await ctx.db
       .query('members')
-      .withIndex('by_workspace_id_user_id', (q) => q.eq('workspaceId', member.workspaceId).eq('userId', userId));
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', member.workspaceId).eq('userId', userId)
+      );
 
     if (!currentMember) return null;
 
@@ -83,5 +92,105 @@ export const getById = query({
       ...member,
       user,
     };
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id('members'),
+    role: v.union(v.literal('admin'), v.literal('member')),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) throw new Error('Người dùng chưa đăng nhập');
+
+    const member = await ctx.db.get(args.id);
+
+    if (!member) throw new Error('Người dùng không tồn tại');
+
+    const currentMember = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', member.workspaceId).eq('userId', userId)
+      )
+      .unique();
+
+    if (!currentMember || currentMember.role !== 'admin')
+      throw new Error('Người dùng không đủ quyền');
+
+    await ctx.db.patch(args.id, {
+      role: args.role,
+    });
+
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id('members'),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) throw new Error('Người dùng chưa đăng nhập');
+
+    const member = await ctx.db.get(args.id);
+
+    if (!member) throw new Error('Người dùng không tồn tại');
+
+    const currentMember = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', member.workspaceId).eq('userId', userId)
+      )
+      .unique();
+
+    if (!currentMember) throw new Error('Người dùng không đủ quyền');
+
+    if (member.role === 'admin') throw new Error('Admin không thể bị xóa');
+
+    const isSelf = currentMember._id === args.id;
+    const isSelfAdmin = currentMember.role === 'admin';
+
+    if (isSelf && isSelfAdmin)
+      throw new Error('Không thể xóa chính bản thân vì bản thân là admin');
+
+    const [messages, reactions, conversations] = await Promise.all([
+      ctx.db
+        .query('messages')
+        .withIndex('by_member_id', (q) => q.eq('memberId', member._id))
+        .collect(),
+      ctx.db
+        .query('reactions')
+        .withIndex('by_member_id', (q) => q.eq('memberId', member._id))
+        .collect(),
+      ctx.db
+        .query('conversations')
+        .filter((q) =>
+          q.or(
+            q.eq(q.field('memberOneId'), member._id),
+            q.eq(q.field('memberTwoId'), member._id)
+          )
+        )
+        .collect(),
+    ]);
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
+
+    for (const reaction of reactions) {
+      await ctx.db.delete(reaction._id);
+    }
+
+    for (const conversation of conversations) {
+      await ctx.db.delete(conversation._id);
+    }
+
+    await ctx.db.delete(args.id);
+
+    return args.id;
   },
 });
--- src/features/members/components/Profile.tsx
+++ src/features/members/components/Profile.tsx
@@ -1,10 +1,30 @@
 import { useGetMember } from '@/features/members/api/useGetMember';
 import { Id } from '../../../../convex/_generated/dataModel';
 import { Button } from '@/components/ui/button';
-import { AlertTriangle, Loader, MailIcon, XIcon } from 'lucide-react';
+import {
+  AlertTriangle,
+  ChevronDown,
+  Loader,
+  MailIcon,
+  XIcon,
+} from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Separator } from '@/components/ui/separator';
 import Link from 'next/link';
+import { useUpdateMember } from '@/features/members/api/useUpdateMember';
+import { useRemoveMember } from '@/features/members/api/useRemoveMember';
+import { useCurrentMember } from '@/features/members/api/useCurrentMember';
+import { useWorkspaceId } from '@/hooks/useWorkspaceId';
+import { toast } from 'sonner';
+import { useConfirm } from '@/hooks/useConfirm';
+import { useRouter } from 'next/navigation';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 interface ProfileProps {
   memberId: Id<'members'>;
@@ -12,11 +32,96 @@ interface ProfileProps {
 }
 
 const Profile = ({ memberId, onClose }: ProfileProps) => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+
+  const [UpdateDialog, confirmUpdate] = useConfirm(
+    'Thay đổi quyền người dùng',
+    'Bạn có chắc muốn thay đổi quyền của người này?'
+  );
+
+  const [LeaveDialog, confirmLeave] = useConfirm(
+    'Rời khỏi workspace',
+    'Bạn có chắc muốn rời khỏi workspace này?'
+  );
+
+  const [RemoveDialog, confirmRemove] = useConfirm(
+    'Xóa thành viên khỏi workspace',
+    'Bạn có chắc muốn xóa người này khỏi workspace?'
+  );
+
+  const { data: currentMember, isLoading: isLoadingCurrentMember } =
+    useCurrentMember({
+      workspaceId,
+    });
+
   const { data: member, isLoading: isLoadingMember } = useGetMember({
     id: memberId,
   });
 
-  if (isLoadingMember) {
+  const { mutate: updateMember, isPending: isUpdatingMember } =
+    useUpdateMember();
+
+  const { mutate: removeMember, isPending: isRemovingMember } =
+    useRemoveMember();
+
+  const onRemove = async () => {
+    const ok = await confirmRemove();
+
+    if (!ok) return;
+
+    removeMember(
+      { id: memberId },
+      {
+        onSuccess: () => {
+          toast.success('Thành viên đã được xóa khỏi nhóm');
+          onClose();
+        },
+        onError: () => {
+          toast.error('Có lỗi xảy ra! xóa thất bại');
+        },
+      }
+    );
+  };
+
+  const onLeave = async () => {
+    const ok = await confirmLeave();
+
+    if (!ok) return;
+    removeMember(
+      { id: memberId },
+      {
+        onSuccess: () => {
+          router.replace('/');
+          toast.success('Bạn đã rời khỏi workspace');
+          onClose();
+        },
+        onError: () => {
+          toast.error('Có lỗi xảy ra! rời khỏi workspace thất bại');
+        },
+      }
+    );
+  };
+
+  const onUpdate = async (role: 'admin' | 'member') => {
+    const ok = await confirmUpdate();
+
+    if (!ok) return;
+    updateMember(
+      { id: memberId, role },
+      {
+        onSuccess: () => {
+          toast.success('Quyền người dùng đã thay đổi');
+          onClose();
+        },
+        onError: () => {
+          toast.error('Có lỗi xảy ra! thay đổi thất bại');
+        },
+      }
+    );
+  };
+
+  if (isLoadingMember || isLoadingCurrentMember) {
     return (
       <div className="h-full flex flex-col">
         <div className="flex justify-between items-center px-4 h-[49px] border-b">
@@ -57,53 +162,106 @@ const Profile = ({ memberId, onClose }: ProfileProps) => {
   const avatarFallback = member.user.name?.[0].toUpperCase() || 'M';
 
   return (
-    <div className="h-full flex flex-col">
-      <div className="flex justify-between items-center px-4 h-[49px] border-b">
-        <p className="text-lg font-bold">Hồ sơ</p>
-        <Button
-          onClick={onClose}
-          size={'iconSm'}
-          variant={'ghost'}
-          className="mt-4"
-        >
-          <XIcon className="size-5 stroke-[1.5]" />
-        </Button>
-      </div>
-      <div className="flex items-center justify-center p-4">
-        <Avatar className="max-w-56 max-h-56 size-full rounded-md">
-          <AvatarImage
-            className="rounded-md shadow-inner"
-            src={member.user.image}
-          />
-          <AvatarFallback className="bg-fuchsia-600 aspect-square text-white shadow-md rounded-md text-6xl">
-            {avatarFallback}
-          </AvatarFallback>
-        </Avatar>
-      </div>
-      <div className="flex flex-col p-4 text-xl font-bold">
-        {member.user.name}
-      </div>
-      <Separator />
-      <div className="flex flex-col p-4">
-        <p className="text-sm font-bold my-4">Thông tin liên hệ</p>
-        <div className="flex items-center gap-2">
-          <div className="size-9 rounded-md bg-muted flex items-center justify-center">
-            <MailIcon className="size-4" />
-          </div>
-          <div className="flex flex-col">
-            <p className="text-sm font-semibold text-muted-foreground">Email</p>
-            <Link
-              href={`https://mail.google.com/mail/?view=cm&fs=1&to=${member.user.email}`}
-              className="text-sm hover:underline text-indigo-600"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {member.user.email}
-            </Link>
+    <>
+      <RemoveDialog />
+      <LeaveDialog />
+      <UpdateDialog />
+      <div className="h-full flex flex-col">
+        <div className="flex justify-between items-center px-4 h-[49px] border-b">
+          <p className="text-lg font-bold">Hồ sơ</p>
+          <Button
+            onClick={onClose}
+            size={'iconSm'}
+            variant={'ghost'}
+            className="mt-4"
+          >
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex items-center justify-center p-4">
+          <Avatar className="max-w-56 max-h-56 size-full rounded-md">
+            <AvatarImage
+              className="rounded-md shadow-inner"
+              src={member.user.image}
+            />
+            <AvatarFallback className="bg-fuchsia-600 aspect-square text-white shadow-md rounded-md text-6xl">
+              {avatarFallback}
+            </AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="flex flex-col p-4 text-xl font-bold">
+          <p className="text-xl font-bold">{member.user.name}</p>
+          {currentMember?.role === 'admin' &&
+          currentMember?._id !== memberId ? (
+            <div className="flex items-center gap-2 mt-4">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant={'outline'} className="w-full capitalize">
+                    {member.role === 'admin' ? 'Quản trị viên' : 'Thành viên'}
+                    <ChevronDown className="size-4 ml-2" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-full">
+                  <DropdownMenuRadioGroup
+                    value={member.role}
+                    onValueChange={(role) =>
+                      onUpdate(role as 'admin' | 'member')
+                    }
+                  >
+                    <DropdownMenuRadioItem value="admin">
+                      Quản trị viên
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="member">
+                      Thành viên
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <Button
+                onClick={onRemove}
+                variant={'destructive'}
+                className="w-full"
+              >
+                Xóa khỏi workspace
+              </Button>
+            </div>
+          ) : currentMember?._id === memberId &&
+            currentMember.role !== 'admin' ? (
+            <div className="mt-4">
+              <Button
+                variant={'destructive'}
+                className="w-full"
+                onClick={onLeave}
+              >
+                Rời khỏi workspace
+              </Button>
+            </div>
+          ) : null}
+        </div>
+        <Separator />
+        <div className="flex flex-col p-4">
+          <p className="text-sm font-bold my-4">Thông tin liên hệ</p>
+          <div className="flex items-center gap-2">
+            <div className="size-9 rounded-md bg-muted flex items-center justify-center">
+              <MailIcon className="size-4" />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-sm font-semibold text-muted-foreground">
+                Email
+              </p>
+              <Link
+                href={`https://mail.google.com/mail/?view=cm&fs=1&to=${member.user.email}`}
+                className="text-sm hover:underline text-indigo-600"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {member.user.email}
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
